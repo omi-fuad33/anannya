358b019f39e6090e52eb9503cd45feeef3c2028b
Fuad Tanvir <omi.fuad33@gmail.com>
2018-07-05 16:43:35 +0600

    Finished linking the stylesheets and bootstrap

--- wp-content/themes/anannya-theme/functions.php
+++ wp-content/themes/anannya-theme/functions.php
@@ -126,11 +126,17 @@ function anannya_scripts() {
     
     wp_enqueue_style ('naeeb-style', get_template_directory_uri().'/css/naeeb.css');
     
-     wp_enqueue_style ('dhrubo-style', get_template_directory_uri().'/css/dhrubo.css');
+    wp_enqueue_style ('dhrubo-style', get_template_directory_uri().'/css/dhrubo.css');
+    
+    wp_enqueue_style ('bootstrap-style', get_template_directory_uri().'/bootstrap/css/bootstrap.min.css');
 
 	wp_enqueue_script( 'anannya-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'anannya-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+    
+    //wp_enqueue_script( 'jquerry-library', get_template_directory_uri() . '/js/jquerry.js', array(), '20151215', true );
+    
+    wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/bootstrap/js/bootstrap.min.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
