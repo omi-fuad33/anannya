8882e3dbe654334e849ad92fbb310d3a5a63d178
Hasan Al Banna <rizbanulhasan08@gmail.com>
2018-07-21 11:58:33 +0600

    archive page done with pagination

--- wp-content/themes/anannya-theme/template-parts/content-blog.php
+++ wp-content/themes/anannya-theme/template-parts/content-blog.php
@@ -4,7 +4,7 @@
                 </figure>
                 <div class="d_text_box">
                     <h3 class="d_heading_text"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-                 <p><?php echo the_content(); ?></p>
+                 <p><?php echo the_excerpt(); ?></p> <!--get_excerpt(140, 'content');-->
                     <p> <img class="d_clock_image" src="http://localhost/anannya/wp-content/uploads/2018/07/clock.png"></p>
                     <p class="d_post_date"> <?php the_time(get_option('date_format')); ?> </p>    
                 </div>
